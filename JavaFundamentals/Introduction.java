2d0e13d63a011c8f64402f3fbd23d843f6eb2cf7
AnanyaShukla0007 <ananyashukla0307@gmail.com>
2025-06-24 10:42:01 +0530

    Remove Array folder from core_java branch

--- JavaFundamentals/Introduction.java
+++
@@ -1,6 +0,0 @@
-public class Introduction {
-	public static void main(String argd[]) {
-		System.out.println("Welcome to Bridgelabz!");
-	}
-
-}
--- JavaFundamentals/VolumeOfCylinder.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class VolumeOfCylinder {
-	public static void main(String args[]) {
-		Scanner sc=new Scanner(System.in);
-		int r=sc.nextInt(),h=sc.nextInt();
-		System.out.println(3.14*(r*r)*h);
-        sc.close();
-	}
-}
