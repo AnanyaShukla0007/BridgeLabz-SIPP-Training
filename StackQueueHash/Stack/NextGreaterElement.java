2154774215f6b813fa472fef8859e25839588847
AnanyaShukla0007 <ananyashukla0307@gmail.com>
2025-07-09 23:52:18 +0530

    Added: Calculating next greater element using Stack

---
+++ StackQueueHash/Stack/NextGreaterElement.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class NextGreaterElement {
+    public static int[] nextGreater(int[] nums) {
+        int[] result = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums[i]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums[i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
+        int[] result = nextGreater(nums);
+        for (int r : result) System.out.print(r + " ");
+    }
+}
