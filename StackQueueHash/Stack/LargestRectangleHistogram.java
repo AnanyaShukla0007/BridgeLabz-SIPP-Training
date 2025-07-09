caa796a238b9bb054a26afd0477ad45668e8f069
AnanyaShukla0007 <ananyashukla0307@gmail.com>
2025-07-09 23:51:25 +0530

    Added: Largest Rectangular Histogram using Stack

---
+++ StackQueueHash/Stack/LargestRectangleHistogram.java
@@ -0,0 +1,26 @@
+mport java.util.*;
+public class LargestRectangleHistogram {
+    public static int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n) ? 0 : heights[i];
+            while (!stack.isEmpty() && h < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] heights = new int[n];
+        for (int i = 0; i < n; i++) heights[i] = sc.nextInt();
+        System.out.println(largestRectangleArea(heights));
+    }
+}
